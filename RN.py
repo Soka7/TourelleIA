613a71429b725226426ec8180ba69bd0a3e59aa7
jesus-dev7 <kids.anguita@gmail.com>
2025-09-30 20:08:42 +0200

    Lier des neurones.

--- RN.py
+++ RN.py
@@ -1,25 +1,17 @@
 import random
 
-Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789&é"
-print(len(Alphabet))
-al = []
-for le in Alphabet:
-    al.append(le)
-
-def LettreAleatoire(al):
-    rdm = random.randint(0, len(al)-1)
-    lett = al[rdm]
-    del(al[rdm])
-    return(lett)
+al = ["Neurone1", "Neurone2", "Neurone3", "Neurone4", "Neurone5", "Neurone6"]
 
 class Reseau:
     def __init__(self):
         self.reseau = []
+        self.A = ""
+        self.B = ""
     
     def AjouterNeurone(self):
-        lettre = LettreAleatoire(al)
-        self.reseau.append([lettre])
-        self.reseau[0].append(lettre)
+        self.reseau.append([al[0]])
+        self.reseau[0].append(al[0])
+        al.remove(al[0])
         for element in range(1, len(self.reseau)):
             while len(self.reseau[element]) != len(self.reseau):
                 self.reseau[element].append("0")
@@ -28,14 +20,41 @@ class Reseau:
         self.reseau.append(["Or"])
         
     def Lier(self):
-        pass
+        #Lier A en fonction de B.(Plan x, y avec origine en haut a gauche)
+        compteur = 0
+        for Neu in self.reseau[0]:
+            if Neu == self.A:
+                Lie = Neu
+            else:
+                compteur += 1
+        for Neur in self.reseau:
+            if Neur[0] == self.B:
+                for ran in range(len(Neur)):
+                    if self.reseau[0][ran] == Lie:
+                        Neur[ran] = 1
+        
+        #Lier B en fonction de A.(Plan x, y avec origine en haut a gauche)          
+        compteur = 0
+        for Neu in self.reseau[0]:
+            if Neu == self.B:
+                Lie = Neu
+            else:
+                compteur += 1
+        for Neur in self.reseau:
+            if Neur[0] == self.A:
+                for ran in range(len(Neur)):
+                    if self.reseau[0][ran] == Lie:
+                        Neur[ran] = 1
 
 Reseau1 = Reseau()
 
 Reseau1.AddOrigin()
-Reseau1.AjouterNeurone()
 for _ in range(6):
     Reseau1.AjouterNeurone()
+
+Reseau1.A = "Neurone6"
+Reseau1.B = "Neurone3"
+Reseau1.Lier()
 
 for n in Reseau1.reseau:
     print(n)
